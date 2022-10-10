3370387ae86f6754da7a76666b8ba0ef840b9654
Kevin <falcony45@gmail.com>
2022-10-09 23:34:43 -0500

    Animaciones agregadas, listas para configurar

---
+++ cofre.js
@@ -0,0 +1,29 @@
+// let cofreInput = document.getElementById("cofre")
+let containerCofre = document.getElementById("cofre-box")
+let lutherBox = document.getElementById("luther")
+let lutherTexto = document.getElementById("lutherTexto")
+
+// cofreInput.checked = false;
+
+
+// cofre.addEventListener("click", cofreAbierto);
+
+// function cofreAbierto(){
+//     if (cofreInput.checked) {
+//         containerCofre.classList.toggle("ocultar")
+//         lutherBox.style.display = "block";
+//         lutherTexto.style.display = "flex"
+//     }
+// }
+
+let gifEnlace = document.getElementById("gif-link");
+
+gifEnlace.addEventListener("click", cofreAbierto)
+
+
+function cofreAbierto() {
+    // gifEnlace.classList.toggle("ocultar")
+    // containerCofre.classList.toggle("ocultar");
+    // lutherBox.style.display = "block";
+    // lutherTexto.style.display = "flex"
+}
