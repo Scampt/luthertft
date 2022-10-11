f85ff2805f012eb22d7924e1eeced85251c84cf3
Luis Kevin <luis.kevin.quiroz@gmail.com>
2022-10-11 15:57:59 -0500

    Terminamos animacion del cofre. (Arreglar texto luther)

--- cofre.js
+++ cofre.js
@@ -1,29 +1,21 @@
-// let cofreInput = document.getElementById("cofre")
-let containerCofre = document.getElementById("cofre-box")
-let lutherBox = document.getElementById("luther")
-let lutherTexto = document.getElementById("lutherTexto")
-
-// cofreInput.checked = false;
-
-
-// cofre.addEventListener("click", cofreAbierto);
-
-// function cofreAbierto(){
-//     if (cofreInput.checked) {
-//         containerCofre.classList.toggle("ocultar")
-//         lutherBox.style.display = "block";
-//         lutherTexto.style.display = "flex"
-//     }
-// }
-
+let cofreContenedor = document.getElementById("cofre-box")
 let gifEnlace = document.getElementById("gif-link");
+let luther = document.getElementById("luther");
+let lutherTexto = document.getElementById("lutherTexto");
+let linkHub = document.getElementById("hub");
+
 
 gifEnlace.addEventListener("click", cofreAbierto)
 
 
 function cofreAbierto() {
-    // gifEnlace.classList.toggle("ocultar")
-    // containerCofre.classList.toggle("ocultar");
-    // lutherBox.style.display = "block";
-    // lutherTexto.style.display = "flex"
+    gifEnlace.classList.toggle("ocultarGif");
+    gifEnlace.style.opacity = 0;
+    gifEnlace.classList.toggle("quitarCofre");
+    luther.classList.toggle("mostrarLuther");
+    luther.style.opacity = 1;
+    lutherTexto.classList.toggle("mostrarLutherTexto");
+    lutherTexto.style.opacity = 1;
+    linkHub.classList.toggle("mostrarHub")
+    // linkHub.style.display = "block"
 }
