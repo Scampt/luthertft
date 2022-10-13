85e2bbcf154a12f3b6f80b04420af92205bf2279
Kevin <falcony45@gmail.com>
2022-10-12 20:43:22 -0500

    Terminamos el hub. SHEESH

--- cofre.js
+++ cofre.js
@@ -13,11 +13,14 @@ function cofreAbierto() {
     gifEnlace.classList.toggle("ocultarGif");
     gifEnlace.style.opacity = 0;
     gifEnlace.classList.toggle("quitarCofre");
+
     luther.classList.toggle("mostrarLuther");
     luther.style.opacity = 1;
     lutherTexto.classList.toggle("mostrarLutherTexto");
     lutherTexto.style.opacity = 1;
 
-    linkHub.classList.toggle("mostrarHub")
+    contenedorHub.classList.toggle("mostrarHub")
+    linkHub.style.color = "#fff"
+    linkHub.style.fontSize = "50px"
 
 }
